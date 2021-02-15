ea1241af77350c5de4a4ae29bc9a6c6f4ee19f59
Urgith <patrykolejniczak7@gmail.com>
2021-02-15 16:12:33 +0100

    czwarte poprawki

--- zad4.py
+++ zad4.py
@@ -1,4 +1,4 @@
-import turtle
+import turtle  # module which draw things
 
 
 def setup():
@@ -7,42 +7,43 @@ def setup():
         Arguments: none of them
         return: None
     '''
-    turtle.hideturtle()
     turtle.speed(0)
+    turtle.hideturtle()
     turtle.up()
     turtle.setposition(-turtle.window_width() / 2, 0)
     turtle.down()
 
 
-def koch_curve(iterations, size, angle, moves='f'):
+def koch_curve(iterations, length, angle, moves='f'):
     ''' function which draw koch curve
         Arguments:
           iterations - number of iterations of koch curve
-          size - length of single line
-          moves - moves to do
+          length - length of single line
+          angle - angle of turn
+          moves - moves to do f-forward, l-turn left, r-turn right
         return: None
     '''
-    if size is None:
-        size = turtle.window_width() / (3**iterations)
+    if length is None:
+        length = turtle.window_width() / (3**iterations)
 
     for move in moves:
         if move == 'f':
             if iterations == 0:
-                turtle.forward(size)
+                turtle.forward(length)
             else:
-                koch_curve(iterations - 1, size, angle, 'flfrflf')
+                koch_curve(iterations - 1, length, angle, 'flfrflf')
         if move == 'l':
             turtle.left(angle)
         if move == 'r':
             turtle.right(2*angle)
 
 
-def draw_koch_curve(iterations, size=None, angle=60):
+def draw_koch_curve(iterations, length=None, angle=60):
     ''' function which draw koch curve with preparation
         Arguments:
           iterations - number of iterations of koch curve
-          size - length of single line
-          angle - angle of curve, nominal=60
+          length - length of single line
+          angle - angle of turn, nominal=60
         return: None
     '''
     if type(iterations) != int:
@@ -50,15 +51,16 @@ def draw_koch_curve(iterations, size=None, angle=60):
     elif iterations < 0:
         raise ValueError('iterations can not be negative')
 
-    if size is None:
+    if length is None:
         pass
-    elif type(size) != int and type(size) != float:
-        raise TypeError('size must be integer or float')
-    elif size < 0:
-        raise ValueError('size can not be negative')
+    elif type(length) != int and type(length) != float:
+        raise TypeError('length must be integer or float')
+    elif length < 0:
+        raise ValueError('length can not be negative')
 
     setup()
-    koch_curve(iterations, size, angle)
+    koch_curve(iterations, length, angle)
     turtle.mainloop()
 
-draw_koch_curve(4)
+if __name__ == '__main__':
+    draw_koch_curve(4)
