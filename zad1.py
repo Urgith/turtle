c73c52fad6e2caae4b5740c44426779d0a22b7e0
Urgith <patrykolejniczak7@gmail.com>
2021-02-15 15:55:02 +0100

    pierwsze trzecia

--- zad1.py
+++ zad1.py
@@ -1,7 +1,7 @@
 from time import perf_counter  # time measurement module
+from scipy import linalg  # module which solve linear equation system
 import numpy as np  # module to work with matrixes
 import pylab  # module which draw charts
-from scipy import linalg
 
 
 def calculating(size_of_matrix):
@@ -18,7 +18,8 @@ def calculating(size_of_matrix):
 
     # A - coefficients of the equation system
     # B - constant terms
-    while perf_counter() - start < 0.05:
+    while perf_counter() - start < 2:
+
         A = np.random.rand(size_of_matrix, size_of_matrix)
         B = np.random.rand(size_of_matrix, 1)
 
@@ -29,10 +30,7 @@ def calculating(size_of_matrix):
         X = linalg.solve(A, B)
         times.append(perf_counter() - start_of_calculating)
 
-    if len(times) > 0:
-        times.remove(max(times))
-
-    return sum(times) / len(times)
+    return min(times)
 
 
 def creating_chart(size, order=3):
@@ -46,7 +44,7 @@ def creating_chart(size, order=3):
     '''
     if type(size) != int:
         raise TypeError('size must be integer')
-    elif size <= 10:
+    elif size <= 1:
         raise ValueError('size must be greater than 11')
 
     if type(order) != int:
@@ -54,17 +52,17 @@ def creating_chart(size, order=3):
     elif order < 0:
         raise ValueError('order can not be negative')
 
-    x_every_10 = np.arange(1, size)
-    y_every_10 = np.asarray([calculating(siz) for siz in np.nditer(x_every_10)])
-    pylab.plot(x_every_10, y_every_10, 'o', markersize=1, label='measured execution time')
+    sizes = np.arange(1, size, 10)
+    execut_t = np.asarray([calculating(siz) for siz in np.nditer(sizes)])
+    pylab.plot(sizes, execut_t, 'o', markersize=1, label='execution time')
 
-    coefs = np.polyfit(x_every_10, y_every_10, 3)
+    coefs = np.polyfit(sizes, execut_t, 3)
     polynomial = np.poly1d(coefs)
 
-    x_every_1 = np.arange(1, size)
-    y_approx = np.asarray([polynomial(siz) for siz in np.nditer(x_every_1)])
+    sizes = np.arange(1, size)
+    y_approx = np.asarray([polynomial(siz) for siz in np.nditer(sizes)])
 
-    pylab.plot(x_every_1, y_approx, label='approximated function')
+    pylab.plot(sizes, y_approx, label='approximated function')
 
     pylab.legend()
     pylab.grid(True)
@@ -75,5 +73,6 @@ def creating_chart(size, order=3):
 
     return polynomial
 
-function = creating_chart(501)
-print(function)
+if __name__ == '__main__':
+    function = creating_chart(502)
+    print(function)
